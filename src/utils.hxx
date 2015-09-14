1e0a2d20015e5a8725b0ee9437ee25cfe6615f97
Feodor Alexeev <feodor.alexeev@gmail.com>
2015-09-14 06:18:05 +0300

    Add UniqueArray class

--- src/utils.hxx
+++ src/utils.hxx
@@ -13,6 +13,24 @@ private:
     void operator=(const Noncopyable&);
 };
 
+template<class T>
+class UniqueArray
+{
+public:
+    explicit UniqueArray(unsigned long size):
+        data(new T[size]) 
+    {}
+
+    ~UniqueArray()
+    { delete[] data; }
+
+    T& operator[](unsigned long at)
+    { return data[at]; }
+
+public:
+    T* const data;
+};
+
 #ifndef NULL
 #define NULL 0
 #endif
