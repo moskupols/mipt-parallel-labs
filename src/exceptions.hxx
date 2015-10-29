be6ec984fb4a62578418064bc4686facdaff9d1f
Feodor Alexeev <feodor.alexeev@gmail.com>
2015-10-29 03:43:50 +0300

    Add sane constructor for CLevelException

--- src/exceptions.hxx
+++ src/exceptions.hxx
@@ -18,6 +18,10 @@ public:
     explicit CLevelException(const std::string& what):
         std::runtime_error(what)
     {}
+
+    explicit CLevelException(const char* what):
+        std::runtime_error(what)
+    {}
 };
 
 class InputError : public std::runtime_error
